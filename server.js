d32b9dce665c526ed0b03f79320560397ccea635
lmk050 <Mukai@dhcp-10-105-246-115.wireless.northwestern.private>
2016-01-13 16:14:39 -0600

    Added Express (restful)

--- server.js
+++ server.js
@@ -1,44 +1,31 @@
-var mysql = require("mysql");
+var express = require('express');
+var app = express();
 
-// First you need to create a connection to the db
-var con = mysql.createConnection({
-  host: "us-cdbr-azure-central-a.cloudapp.net",
-  user: "b443fc80dd2566",
-  password: "4d39195d",
-  database: "coffeechat"
+
+
+app.get('/', function (req, res) {
+  res.send('Welcome to NU Coffee Chat!');
 });
 
-con.connect(function(err){
-  if(err){
-    console.log('Error connecting to Db');
-    return;
-  }
-  console.log('Connection established');
+var fs = require('fs');
+var resource = null;
+
+fs.readFile('./Resources/resources.txt', function(err, data) {
+    if(err) throw err;
+    var array = data.toString().split("\n");
+    for(i in array) {
+        console.log(array[i]);
+        resource=require(array[i]);
+        app.get('/'+resource.path, function (req, res) {
+            resource.handle(req,res);
+        });
+    }
 });
 
-var str='';
-con.query('SELECT id, first, last, age FROM TESTTABLE',function(err,rows){
-  if(err) throw err;
 
-  console.log('Data received from Db:\n');
-  console.log(rows);
 
-  for (var i = 0; i < rows.length; i++) {
-   str+=rows[i].id+','+rows[i].first+','+rows[i].last+','+rows[i].age+'\n';
-  };
-});
-
-con.end(function(err) {
-  // The connection is terminated gracefully
-  // Ensures all previously enqueued queries are still
-  // before sending a COM_QUIT packet to the MySQL server.
-});
-
-var http = require('http')
 var port = process.env.PORT || 1337;
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World, NUCoffeeChat 2\n'+str);
-}).listen(port);
 
-var mysql = require("mysql");
+app.listen(port, function () {
+  console.log('Example app listening on port port!');
+})
